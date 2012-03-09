e0d8b731883a1ac6bd3742c336c881e4a380b1aa
Jay Roberts <jay@gloryfish.org>
2012-03-09 10:30:12 -0500

    Clean up code

--- monitor.py
+++ monitor.py
@@ -1,68 +1,82 @@
+#! /usr/bin/python
+
+# 
+#  monitor.py
+#  plate-detection
+#  
+#  Created by Jay Roberts on 2012-01-25.
+#  Copyright 2012 GloryFish.org. All rights reserved.
+# 
+
 import sys
 import cv
 import pygame
 
 class Monitor:
-    
-    
+"""Detects circular shapes in images captured form a webcam. Provides a set of images used in the
+   capture process for display. Also provides a list of the position and size of detetced circles"""    
+
     def __init__(self):
         self.threshhold = 200
-        self.save_images = False
-        self.display_type = 'original'
+        self.images = dict()
         self.camera = cv.CaptureFromCAM(-1)
+        self.circles = []
     
-    def capture(self, filename = None):
-        self.image = None
+    def get_image(self, image_name):
+        """Converts the named image from the OpenCV format to one usable by PyGame"""
+        image = self.images[image_name]
         
-        if filename == None:
-            # capture from open cv
-            self.image = cv.QueryFrame(self.camera)
-        else:
-            self.image = cv.LoadImage(filename)
+        source_image = cv.CreateMat(self.images[image_name].height, self.images[image_name].width, cv.CV_8UC3)
+        if image_name == 'gray' or image_name == 'edges':
+            cv.CvtColor(image, source_image, cv.CV_GRAY2RGB)
+        else:        
+            cv.CvtColor(image, source_image, cv.CV_BGR2RGB)
+
+        return pygame.image.frombuffer(source_image.tostring(), cv.GetSize(source_image), "RGB")
+    
+    def capture(self):
+        """Capture a frame from the camera, perform the Hough tranform and store the results."""
+        self.images['original'] = cv.QueryFrame(self.camera)
         
-        self.output = cv.CloneImage(self.image)
-        self.gray = cv.CreateImage(cv.GetSize(self.image), cv.IPL_DEPTH_8U, 1)
-        self.edges = cv.CreateImage(cv.GetSize(self.image), cv.IPL_DEPTH_8U, 1)
+        self.images['output'] = cv.CloneImage(self.images['original'])
+        self.images['gray'] = cv.CreateImage(cv.GetSize(self.images['original']), cv.IPL_DEPTH_8U, 1)
+        self.images['edges'] = cv.CreateImage(cv.GetSize(self.images['original']), cv.IPL_DEPTH_8U, 1)
     
         # Create a grayscale version
-        cv.CvtColor(self.image, self.gray, cv.CV_BGR2GRAY)
+        cv.CvtColor(self.images['original'], self.images['gray'], cv.CV_BGR2GRAY)
 
     
         # Create an edge-detected version
-        cv.Canny(self.gray, self.edges, self.threshhold, self.threshhold / 2, 3)
+        cv.Canny(self.images['gray'], self.images['edges'], self.threshhold, self.threshhold / 2, 3)
 
         hough_results = cv.CreateMat(640, 1, cv.CV_32FC3)
         
-        cv.HoughCircles(self.gray, hough_results, cv.CV_HOUGH_GRADIENT, 2, self.gray.width / 18, self.threshhold, 300, 0, 0)
+        cv.HoughCircles(self.images['gray'], hough_results, cv.CV_HOUGH_GRADIENT, 2, self.images['gray'].width / 18, self.threshhold, 300, 0, 0)
 
+        self.circles = []
+        
         for i in range(hough_results.rows):
             val = hough_results[i, 0]
             center = (int(val[0]), int(val[1]))
             radius = int(val[2])
+            self.circles.append((val, center, radius))
+            # print 'Found circle at %s with size %s' % (str(center), str(radius))
             
-            print 'Found circle at %s with size %s' % (str(center), str(radius))
-            
-            cv.Circle(self.output, center, radius, cv.RGB(255, 0, 0), 2, cv.CV_AA, 0)
-
-        if self.save_images:
-            cv.SaveImage('original.png', self.image)
-            cv.SaveImage('gray.png', self.gray)
-            cv.SaveImage('edges.png', self.edges)
-            cv.SaveImage('output.png', self.output)
+            # cv.Circle(self.output, center, radius, cv.RGB(255, 0, 0), 2, cv.CV_AA, 0)
         
-        if self.display_type == 'original':
-            return self.image
-
-        if display_type == 'gray':
-            return self.gray
-
-        if display_type == 'edges':
-            return self.edges
-
-        if display_type == 'output':
-            return self.output
-        
-        return self.output
+        # if self.display_type == 'original':
+        #     return self.image
+        # 
+        # if display_type == 'gray':
+        #     return self.gray
+        # 
+        # if display_type == 'edges':
+        #     return self.edges
+        # 
+        # if display_type == 'output':
+        #     return self.output
+        # 
+        # return self.output
     
 if __name__ == '__main__':
     
@@ -73,13 +87,15 @@ if __name__ == '__main__':
     
     font = pygame.font.Font(None, 24)
     
-    
     monitor = Monitor()
     
     display_type = 'original'
     
+    draw_circles = False
+    
     clock = pygame.time.Clock()
     finished = False
+    
     while not finished:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -93,23 +109,24 @@ if __name__ == '__main__':
                     display_type = 'edges'
                 if event.key == pygame.K_SPACE:
                     display_type = 'output'
+                if event.key == pygame.K_c:
+                    draw_circles = not draw_circles
                 print display_type
                     
         screen.fill((0, 0, 0))
 
-        # Convert OpenCv data to PyGame surface
-        monitor.display_type = display_type
-        image = monitor.capture()
+        monitor.capture()
         
-        src_rgb = cv.CreateMat(image.height, image.width, cv.CV_8UC3)
-        if display_type == 'gray' or display_type == 'edges':
-            cv.CvtColor(image, src_rgb, cv.CV_GRAY2RGB)
-        else:        
-            cv.CvtColor(image, src_rgb, cv.CV_BGR2RGB)
-
-        pygame_surface = pygame.image.frombuffer(src_rgb.tostring(), cv.GetSize(src_rgb), "RGB")
+        for circle in monitor.circles:
+            print 'Found circle at %s with size %s' % (str(circle[1]), str(circle[2]))
+        
+        pygame_surface = monitor.get_image(display_type)
         
         screen.blit(pygame_surface, (0, 0))
+
+        if draw_circles:
+            for circle in monitor.circles:
+                pygame.draw.circle(screen, (255, 0, 0), (circle[1][0], circle[1][1]), circle[2], 3)
 
         display_text = font.render(display_type, 1, (0, 0, 0))
         screen.blit(display_text, (10, 10))
@@ -119,15 +136,11 @@ if __name__ == '__main__':
 
         #  Render fps
         fps = str(clock.get_fps())
-        fps_text = font.render(fps, 1, (0, 0, 0))
+        fps_text = font.render(fps, 1, (0, 0, 0)) # Black
         screen.blit(fps_text, (10, 30))
 
-        fps_text = font.render(fps, 1, (255, 255, 255))
+        fps_text = font.render(fps, 1, (255, 255, 255)) # White
         screen.blit(fps_text, (11, 31))
-
-
 
         clock.tick(30)
         pygame.display.flip()
-    
-    
